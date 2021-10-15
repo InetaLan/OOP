a5b472618e11128025568ce55c3497b72c8a6aec
InetaLan <91073106+InetaLan@users.noreply.github.com>
2021-10-15 09:28:44 +0300

    Delete countWordsInString.cpp

--- countWordsInString.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "countWordsInString.h"
-
-
-unsigned int countWordsInString(string const& str)
-{
-    std::stringstream stream(str);
-    return std::distance(std::istream_iterator<string>(stream), std::istream_iterator<string>());
-}
